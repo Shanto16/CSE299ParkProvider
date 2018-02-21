4bef33db0cdc1146b1c71eca1db442e62dfa3d15
Shanto16 <shakil.glostars@gmail.com>
2018-02-21 11:30:37 +0600

    sign up page

--- app/src/main/java/nsu/cse299parkprovider/LoginPage.java
+++ app/src/main/java/nsu/cse299parkprovider/LoginPage.java
@@ -1,7 +1,9 @@
 package nsu.cse299parkprovider;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -22,6 +24,13 @@ public class LoginPage extends AppCompatActivity {
         tv_openAcc = findViewById(R.id.open_new_acc);
         et_username = findViewById(R.id.username);
         et_password = findViewById(R.id.password);
+
+        tv_openAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginPage.this,SignUp_Driver.class));
+            }
+        });
 
 
 
---
+++ app/src/main/java/nsu/cse299parkprovider/SignUp_Driver.java
@@ -0,0 +1,13 @@
+package nsu.cse299parkprovider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp_Driver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up__driver);
+    }
+}
