5d0e51785a413c5c3f725c89cb1f14947f271f0c
Shanto16 <shakil.glostars@gmail.com>
2018-03-09 20:03:45 +0600

    Added mapview

--- app/src/main/java/nsu/cse299parkprovider/Map.java
+++ app/src/main/java/nsu/cse299parkprovider/Map.java
@@ -1,32 +1,108 @@
 package nsu.cse299parkprovider;
 
+import android.*;
+import android.Manifest;
 import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class Map extends Fragment {
+public class Map extends Fragment  implements OnMapReadyCallback {
 
-    MapView map;
+
+    private GoogleMap gMap;
+    private MapView mapView;
+
+    private LocationManager locationManager;
+    private static final long MIN_TIME = 400;
+    private static final float MIN_DISTANCE = 1000;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
 
-        map = rootView.findViewById(R.id.mainMap);
 
-
+        mapView = rootView.findViewById(R.id.mainMap);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
 
 
         return rootView;
     }
+
+
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+
+    }
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng coordinate = new LatLng(23.814784, 90.425669); //Store these lat lng values somewhere. These should be constant.
+        CameraUpdate location = CameraUpdateFactory.newLatLngZoom(
+                coordinate, 18);
+        map.animateCamera(location);
+        map.addMarker(new MarkerOptions().position(new LatLng(23.814784,  90.425669)).title("Marker"));
+
+
+    }
+    @Override
+    public void onPause() {
+        mapView.onPause();
+        super.onPause();
+        Log.v(this.getClass().getSimpleName(), "onPause()");
+
+    }
+    @Override
+    public void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+
 
 
 }
