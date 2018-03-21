b82048e9cd1922a11ee287fd716ed8c2575602a5
jhanik <34456423+jhanik@users.noreply.github.com>
2018-03-22 02:13:10 +0600

    add latitude and longitude and create database and save data

--- app/src/main/java/nsu/cse299parkprovider/SignupDriver.java
+++ app/src/main/java/nsu/cse299parkprovider/SignupDriver.java
@@ -2,12 +2,82 @@ package nsu.cse299parkprovider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import nsu.cse299parkprovider.R;
+
+import static android.widget.Toast.*;
 
 public class SignupDriver extends AppCompatActivity {
+
+    EditText name;
+    EditText email;
+    EditText password;
+    EditText phone;
+    EditText licencenum;
+    EditText vehicleMake;
+    EditText vehicleModel;
+    Button buttonAdd;
+    DatabaseReference databaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up__driver);
+
+        databaseUser = FirebaseDatabase.getInstance().getReference("user");
+
+        name = (EditText) findViewById(R.id.name);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        phone = (EditText) findViewById(R.id.phone);
+        licencenum = (EditText) findViewById(R.id.licenceNum);
+
+        vehicleMake = (EditText) findViewById(R.id.vehicleMake);
+        vehicleModel = (EditText) findViewById(R.id.vehicleModel);
+        buttonAdd = (Button) findViewById(R.id.signup);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addUser();
+            }
+        });
+
     }
+    private void addUser(){
+
+        String Name = name.getText().toString().trim();
+        String Email = email.getText().toString().trim();
+        String Password = password.getText().toString().trim();
+        String Phone = phone.getText().toString().trim();
+        String VehicleMake = vehicleMake.getText().toString().trim();
+        String VehicleModel = vehicleModel.getText().toString().trim();
+        String Licencenum = licencenum.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(Name) && !TextUtils.isEmpty(Email) && !TextUtils.isEmpty(Password) && !TextUtils.isEmpty(Phone) && !TextUtils.isEmpty(VehicleMake)
+                && !TextUtils.isEmpty(VehicleModel) && !TextUtils.isEmpty(Licencenum)){
+
+            user user1 = new user(Name,Email, Password, Phone,VehicleMake,VehicleModel,Licencenum);
+            databaseUser.child(Phone).setValue(user1);
+            makeText(this, "USer Added Successfully", LENGTH_SHORT).show();
+        }
+        else makeText(this, "Please fill up all the fields properly", LENGTH_SHORT).show();
+
+
+
+
+
+
+    }
+
+
+
+
 }
--- app/src/main/java/nsu/cse299parkprovider/SignupProvider.java
+++ app/src/main/java/nsu/cse299parkprovider/SignupProvider.java
@@ -2,15 +2,41 @@ package nsu.cse299parkprovider;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-/**
- * Created by Shanto on 3/14/2018.
- */
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static android.widget.Toast.LENGTH_SHORT;
+import static android.widget.Toast.makeText;
+
 
 public class SignupProvider extends Fragment {
+
+
+    EditText email;
+    EditText password;
+    EditText phoneno;
+    Spinner area;
+    EditText latitude;
+    EditText longitude;
+    CheckBox high;
+    CheckBox medium;
+
+    CheckBox low;
+    TextView text;
+    Button provider;
+    DatabaseReference databaseUser;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,7 +45,68 @@ public class SignupProvider extends Fragment {
 
 
 
+        databaseUser = FirebaseDatabase.getInstance().getReference("provider");
+        text = (TextView) rootView.findViewById(R.id.link);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+                email = (EditText) rootView.findViewById(R.id.mEmail);
+        password = (EditText) rootView.findViewById(R.id.mPassword);
+        phoneno = (EditText) rootView.findViewById(R.id.mPhone);
+        latitude = (EditText) rootView.findViewById(R.id.lat);
+        longitude = (EditText) rootView.findViewById(R.id.Long);
+        area = (Spinner) rootView.findViewById(R.id.mArea);
+        high = (CheckBox) rootView.findViewById(R.id.high);
+        medium = (CheckBox) rootView.findViewById(R.id.medium);
+        low = (CheckBox)  rootView.findViewById(R.id.low);
+        provider = (Button) rootView.findViewById(R.id.mbutton);
+
+        provider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addprovider();
+
+            }
+        });
+
+
+
 
         return rootView;
     }
+
+    private  void addprovider(){
+
+
+        String Email = email.getText().toString().trim();
+        String Password = password.getText().toString().trim();
+        String Phone = phoneno.getText().toString().trim();
+        String Area = area.getSelectedItem().toString().trim();
+        String lat = latitude.getText().toString().trim();
+        String Long = longitude.getText().toString().trim();
+
+
+        String High = high.getText().toString().trim();
+        String Medium = medium.getText().toString().trim();
+        String Low = low.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(Email) && !TextUtils.isEmpty(Password) && !TextUtils.isEmpty(Phone) && !TextUtils.isEmpty(Area)
+                && !TextUtils.isEmpty(lat) && !TextUtils.isEmpty(Long) ){
+            String id = databaseUser.push().getKey();
+            provider provider1 = new provider(Email, Password, Phone,Area,lat,Long,High,Medium,Low);
+            databaseUser.child(id).setValue(provider1);
+            makeText(getActivity(), " Provider Added Successfully", LENGTH_SHORT).show();
+        }
+        else makeText(getActivity(), "Please fill up all the fields properly", LENGTH_SHORT).show();
+
+
+
+
+
+    }
+
+
+
+
+
 }
